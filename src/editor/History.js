31a3aa62982b6e0f43a7df685467fe5cfc4dbfb7
stackdump <myork@stackdump.com>
2022-06-05 18:04:32 -0500

    always show history in order

--- src/editor/History.js
+++ src/editor/History.js
@@ -4,15 +4,22 @@ import {FormGroup, Grid, TextField} from "@mui/material";
 
 export default function History(props) {
 
+    const history = props.metaModel.simulation.history;
+    const entries = []
+
+    for (const i in history) {
+        const val = history[i]
+        entries.unshift(
+            <FormGroup row key={"history"+val.seq}>
+                <TextField sx={{ marginTop: "5px", width: "20em" }} label={val.seq} key={"histfield"+val.seq} value={val.action} />
+            </FormGroup>
+        )
+
+    }
+
     return <React.Fragment>
         <Grid container spacing={2}>
-            <Grid item xs={12} md={12}>
-                {props.metaModel.simulation.history.reverse().map((val) =>
-                    <FormGroup row key={"history"+val.seq}>
-                        <TextField sx={{ marginTop: "5px", width: "7em" }} label={val.seq} key={"histfield"+val.seq} value={val.action} />
-                    </FormGroup>,
-                )}
-            </Grid>
+            <Grid item xs={12} md={12}>{entries}</Grid>
         </Grid>
 
     </React.Fragment>
