ba4f3ce800c641b98f27b61d768f590eb0f19878
stackdump <myork@stackdump.com>
2023-12-29 03:25:45 -0600

    adopt pflow.js method for reading file from url

--- src/pflow/permalink.ts
+++ src/pflow/permalink.ts
@@ -1,9 +1,50 @@
 import * as mm from "@pflow-dev/metamodel";
 import JSZip from "jszip";
 
+function getQueryParams(str = window?.location?.search, separator = '&'): Record<string, any> {
+    const obj: Record<string, any> = {};
+    if (str.length === 0) return obj;
+    const c = str.substr(0, 1);
+    const s = c === '?' || c === '#' ? str.substr(1) : str;
+
+    const a = s.split(separator);
+    for (let i = 0; i < a.length; i++) {
+        const p = a[i].indexOf('=');
+        if (p < 0) {
+            obj[a[i]] = '';
+            continue;
+        }
+        let k = decodeURIComponent(a[i].substr(0, p)),
+            v = decodeURIComponent(a[i].substr(p + 1));
+
+        const bps = k.indexOf('[');
+        if (bps < 0) {
+            obj[k] = v;
+            continue;
+        }
+
+        const bpe = k.substr(bps + 1).indexOf(']');
+        if (bpe < 0) {
+            obj[k] = v;
+            continue;
+        }
+
+        const bpv = k.substr(bps + 1, bps + bpe - 1);
+        k = k.substr(0, bps);
+        if (bpv.length <= 0) {
+            if (typeof obj[k] != 'object') obj[k] = [];
+            obj[k].push(v);
+        } else {
+            if (typeof obj[k] != 'object') obj[k] = {};
+            obj[k][bpv] = v;
+        }
+    }
+    return obj;
+}
+
+
 export function loadModelFromPermLink(): Promise<mm.Model> {
-    const params = new URLSearchParams(window.location.search);
-    const zippedJson = params.get('z');
+    const zippedJson = getQueryParams()['z']
     if (zippedJson) {
         return unzip(zippedJson).then((json) => {
             const m = JSON.parse(json) as mm.ModelDeclaration;
@@ -38,5 +79,8 @@ export function unzip(data: string): Promise<string> {
                 });
             }
             return Promise.reject('no model.json found');
+        }).catch((e) => {
+            console.error(e);
+            return Promise.reject(e);
         });
 }
